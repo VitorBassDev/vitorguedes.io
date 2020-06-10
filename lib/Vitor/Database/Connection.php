6e3487174c8cbc20fbb6f60892d9e3d9b78e1219
Vitor Dev <vittorbassdev@gmail.com>
2020-06-10 18:54:16 -0300

    Configuração do botão LOGAR e Configuração do apontamento do banco de dados

--- lib/Vitor/Database/Connection.php
+++ lib/Vitor/Database/Connection.php
@@ -10,7 +10,14 @@ abstract class Connection
 	public static function getConn()
 	{
 		if(!self::$conn){
-			self::$conn = new \PDO('mysql: host=localhost; dbname=portfolio', 'root', '');
+			/**APONTAMENTO PARA O BANCO EM LOCALHOST 
+			* self::$conn = new \PDO('mysql: host=localhost; dbname=portfolio', 'root', '');
+			*/
+			/**APONTAMENTO PARA O BANCO NO SERVIDOR DA 000WEBHOST **/
+			self::$conn = new \PDO('
+									mysql: host=localhost;
+									dbname=id13888523_projetovitor',
+									'id13888523_vitor', 'MAIStecnologia0800@');
 		}
 
 		return self::$conn;
